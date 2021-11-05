a4f49f39cc185e60d8c9af71e87167258e6ee5a5
sarapoza <sara.saldanha.poza@gmail.com>
2021-11-05 07:06:12 -0300

    conclusao autenticacao parte 1

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$Nh7h9RUuNIbK2mCWE39bNOHlhzDSjsssMyVlzvchTrTjVoMHvcDK6');
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
 
